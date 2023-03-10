085a41623bed78bfbfeae9da43e93c0b47c0e67f
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-03-10 12:25:12 +0000

    update

--- src/components/Atoms/Logotype.tsx
+++ src/components/Atoms/Logotype.tsx
@@ -1,30 +1,27 @@
 // MOLECULE
-import {Box} from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
 
-import Link from 'next/link'
-import Image from 'next/image'
-
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Logotype() {
-    return (
-        
-            <Link href='/'>
-                <Box
-                    as="span"
-                    width={{ base: '60px', md: '80px', lg: '100px' }}
-                    display='flex'
-                    justifyContent='center'
-                    alignItems='center'
-                >
-                    <Image
-                        width={90}
-                        height={90}
-                        priority
-                        src='/images/logotype.webp' alt='caosamentos logotype'
-                    />
-                </Box>
-            </Link>
-        
-    )
+	return (
+		<Link href="/">
+			<Box
+				as="span"
+				width={{ base: '60px', md: '80px', lg: '100px' }}
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<Image
+					width={90}
+					height={90}
+					priority
+					src="/images/logotype.webp"
+					alt="caosamentos logotype"
+				/>
+			</Box>
+		</Link>
+	);
 }
-
