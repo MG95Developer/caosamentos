78a0eba3a07101ac7ef88eb00af590a9c44dafe3
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-03-10 15:09:12 +0000

    update

--- pages/racas.tsx
+++ pages/racas.tsx
@@ -9,63 +9,79 @@ export default function Racas() {
 	return (
 		<Box>
 			{/* page title */}
+
 			<Box my={30}>
 				<Heading>Página Raças</Heading>
 			</Box>
 
-			{/* component */}
-			<Box my={30}>
-				<BackToResults title="Voltar aos resultados de pesquisa" URL="/404" />
-			</Box>
-
-			{/* component */}
-			<Box my={30}>
-				<BookmarkOrShare bookmark="guardar nos favoritos" share="partilhar" />
-			</Box>
-
-			{/* component */}
-			<Box my={30}>
-				<DogSize sizeXS="Mini" sizeM="Médio" sizeL="Grande" sizeXL="Gigante" />
-			</Box>
-
-			{/* component */}
-			<Box my={30}>
-				<DogMetrics metricsTitle="energia" percentage={25} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="social" percentage={50} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="inteligência" percentage={60} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="ambiente interior" percentage={30} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="ambiente exterior" percentage={70} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="exercício" percentage={75} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="gromming" percentage={65} />
-			</Box>
-			<Box my={30}>
-				<DogMetrics metricsTitle="esperança média de vida" percentage={85} />
-			</Box>
-
-			{/* component */}
 			<Box my={60}>
-				<DogProfileCard
-					dogIMG="/images/dog-2.jpeg"
-					nameTitle="nome"
-					breedTitle="raça"
-					locationTitle="localização"
-					buttonText="ver o perfil"
-					dogName="Filipe"
-					dogBreed="Pastor Alemão"
-					dogLocation="almancil"
-				/>
+				{/* component */}
+				<Box my={30}>
+					<BackToResults title="Voltar aos resultados de pesquisa" URL="/404" />
+				</Box>
+			</Box>
+
+			<Box my={60}>
+				{/* component */}
+				<Box my={30}>
+					<BookmarkOrShare bookmark="guardar nos favoritos" share="partilhar" />
+				</Box>
+			</Box>
+
+			<Box my={60}>
+				{/* component */}
+				<Box my={30}>
+					<DogSize
+						sizeXS="Mini"
+						sizeM="Médio"
+						sizeL="Grande"
+						sizeXL="Gigante"
+					/>
+				</Box>
+			</Box>
+
+			<Box my={60}>
+				{/* component */}
+				<Box my={30}>
+					<DogMetrics metricsTitle="energia" percentage={25} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="social" percentage={50} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="inteligência" percentage={60} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="ambiente interior" percentage={30} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="ambiente exterior" percentage={70} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="exercício" percentage={75} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="gromming" percentage={65} />
+				</Box>
+				<Box my={30}>
+					<DogMetrics metricsTitle="esperança média de vida" percentage={85} />
+				</Box>
+			</Box>
+
+			<Box my={60}>
+				{/* component */}
+				<Box my={60}>
+					<DogProfileCard
+						dogIMG="/images/dog-2.jpeg"
+						nameTitle="nome"
+						breedTitle="raça"
+						locationTitle="localização"
+						buttonText="ver o perfil"
+						dogName="Filipe"
+						dogBreed="Pastor Alemão"
+						dogLocation="almancil"
+					/>
+				</Box>
 			</Box>
 		</Box>
 	);
